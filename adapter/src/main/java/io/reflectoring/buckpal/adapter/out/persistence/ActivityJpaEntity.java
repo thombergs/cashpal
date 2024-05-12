0b947c1be8cd1a51a5618f4203538775a428b67c
KimDoubleB <tree9295@gmail.com>
2024-05-12 13:37:54 +0900

    feat: covert to multi module

---
+++ adapter/src/main/java/io/reflectoring/buckpal/adapter/out/persistence/ActivityJpaEntity.java
@@ -0,0 +1,40 @@
+package io.reflectoring.buckpal.adapter.out.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "activity")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ActivityJpaEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private LocalDateTime timestamp;
+
+	@Column
+	private long ownerAccountId;
+
+	@Column
+	private long sourceAccountId;
+
+	@Column
+	private long targetAccountId;
+
+	@Column
+	private long amount;
+
+}
---
+++ port/src/main/java/io/reflectoring/buckpal/application/port/out/LoadAccountPort.java
@@ -0,0 +1,11 @@
+package io.reflectoring.buckpal.application.port.out;
+
+import io.reflectoring.buckpal.application.domain.model.Account;
+import io.reflectoring.buckpal.application.domain.model.Account.AccountId;
+
+import java.time.LocalDateTime;
+
+public interface LoadAccountPort {
+
+	Account loadAccount(AccountId accountId, LocalDateTime baselineDate);
+}
